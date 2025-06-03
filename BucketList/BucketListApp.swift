be70373e7fa9f077ef7eda78a094024779729b1e
Daria Yats <dariayatsyniuk@gmail.com>
2025-06-03 16:42:46 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Дарья Яцынюк on 03.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
