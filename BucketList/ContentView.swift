2a7da1ac6f99c9c66d9dca2c897d2ea67f96f87d
Daria Yats <dariayatsyniuk@gmail.com>
2025-06-05 12:44:35 +0200

    initial commit

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,17 +5,46 @@
 //  Created by Дарья Яцынюк on 03.06.2025.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
+    )
+    @State private var viewModel = ViewModel()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MapReader { proxy in
+            Map(initialPosition: startPosition) {
+                ForEach(viewModel.locations) { location in
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                            .onLongPressGesture(minimumDuration: 0.2) {
+                                viewModel.selectedPlace = location
+                            }
+                    }
+                }
+            }
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    viewModel.addLocation(at: coordinate)
+                }
+            }
+            .sheet(item: $viewModel.selectedPlace) { place in
+                EditView(location: place) {
+                    viewModel.update(location: $0)
+                }
+            }
         }
-        .padding()
     }
 }
 
